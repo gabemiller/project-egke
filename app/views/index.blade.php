b73ba8e3160b74ea8dd083d838723912604e4087
Gabor Molnar <gabormolnar92@gmail.com>
2014-12-30 18:41:32 +0100

    Index bugfix

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -32,7 +32,7 @@
         </div>
     </div>
 
-    <div class="row">
+    <!--div class="row">
     <div class="col-xs-12">
         <div class="quote-carousel owl-carousel">
             @for($i=0;$i<10;$i++)
@@ -44,6 +44,6 @@
             @endfor
             </div>
         </div>
-    </div>
+    </div-->
 
 @stop
