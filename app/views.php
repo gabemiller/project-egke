785b677bb90e56adfd2c0100bc537c42ca6e981a
Gabor Molnar <gabormolnar92@gmail.com>
2015-02-09 13:38:14 +0100

    View Composer #1

--- app/views.php
+++ app/views.php
@@ -1,5 +1,9 @@
 <?php
 
 View::composer('index',function($view){
-    $view->with('quotes',\Divide\CMS\Quote::all(['quote','author']));
+    //$view->with('quotes',\Divide\CMS\Quote::all(['quote','author']));
+    $view->with('quotes',\Divide\CMS\Quote::orderByRaw('RAND()')
+        ->take(10)
+        ->remember(1)
+        ->get(['quote','author']));
 });
