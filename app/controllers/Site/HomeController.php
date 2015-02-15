a53f3e889344c6767c99580c7153a7cf0516ae93
Gabor Molnar <gabormolnar92@gmail.com>
2015-02-15 22:35:49 +0100

    Design #4

--- app/controllers/Site/HomeController.php
+++ app/controllers/Site/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends \BaseController {
         $article = Article::where('published', '=', true)
             ->orderBy('created_at', 'DESC')
             ->select(['id', 'title', 'author_id', 'created_at', 'content','gallery_id'])
-            ->take(4)->get();
+            ->take(6)->get();
 
         $this->layout->content = View::make('index')
             ->with('articles', $article);
